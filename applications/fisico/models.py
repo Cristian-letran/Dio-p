7a03aa50194292be781c1369ea08db9fc72456a8
Cristian6600 <cristian6600@hotmail.es>
2022-11-08 11:40:53 -0500

    cod_ins-coreccion-cobertura

--- applications/fisico/models.py
+++ applications/fisico/models.py
@@ -379,7 +379,8 @@ class Cobertura(models.Model):
         self.bolsa.id_est_id  = self.estados
         self.bolsa.mot_id = self.bolsa.mot_id = 3
         self.pdf_cobertura = self.pdf 
-        self.cod_ins = str(self.cod_cliente)
+        self.bolsa.cod_ins.id = str(self.cod_cliente)
+        
         
         self.bolsa.fecha_visita = datetime.datetime.now()
         self.bolsa.fecha_recepcion = datetime.datetime.now()
