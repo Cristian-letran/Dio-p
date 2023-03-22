6ed2e9192873b5f1e7a52facbc7550f9912270da
Cristian6600 <cristian6600@hotmail.es>
2023-03-22 16:02:38 -0500

    export-daviplatra

--- applications/daviplata/admin.py
+++ applications/daviplata/admin.py
@@ -16,13 +16,39 @@ from . models import (
 class DaviplataAdmin(ImportExportModelAdmin, admin.ModelAdmin):
    list_display = ('id_ruta', 'nombre_establecimiento', 'direccion_base', 'user')
 
+@admin.register(Red)
+class DaviplataAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+   list_display = ('nombre',)
 
-admin.site.register(Red)
-admin.site.register(TipoNoEfectiva)
-admin.site.register(TipoEstablecimiento)
-admin.site.register(OtroTipoEstablecimiento)
-admin.site.register(TipoSenalizacion)
-admin.site.register(OtroTipoSenalizacion)
-admin.site.register(Acude)
-admin.site.register(AcudeOtro)
-admin.site.register(Medio)
+@admin.register(TipoNoEfectiva)
+class DaviplataAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+   list_display = ('nombre',)
+
+@admin.register(TipoEstablecimiento)
+class DaviplataAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+   list_display = ('nombre',)
+
+@admin.register(OtroTipoEstablecimiento)
+class DaviplataAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+   list_display = ('nombre',)
+
+@admin.register(TipoSenalizacion)
+class DaviplataAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+   list_display = ('nombre',)
+
+@admin.register(OtroTipoSenalizacion)
+class DaviplataAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+   list_display = ('nombre',)
+
+@admin.register(Acude)
+class DaviplataAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+   list_display = ('nombre',)
+
+@admin.register(AcudeOtro)
+class DaviplataAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+   list_display = ('nombre',)
+   
+@admin.register(Medio)
+class DaviplataAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+   list_display = ('nombre',)
+
