1f205dad270c3d78a95b9c7f3d99884be36e6560
Cristian6600 <cristian6600@hotmail.es>
2023-04-14 18:09:44 -0500

    fiulter-ciudad

--- applications/users/admin.py
+++ applications/users/admin.py
@@ -8,7 +8,7 @@ class UserAdmin(ImportExportModelAdmin, admin.ModelAdmin):
     list_display = ('id', 'username', 'email', 'nombres', 'apellidos', 'genero', 'ciudad')
     search_fields = ('username', 'nombres')
     raw_id_fields = ['ciudad']
-    list_filter = ('ocupation', 'cliente')
+    list_filter = ('ocupation', 'cliente', 'ciudad')
 
 @admin.register(Areas)
 class AreasAdmin(ImportExportModelAdmin, admin.ModelAdmin):
