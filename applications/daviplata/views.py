d1d5971c9692fc1f5e5ac4e339f6254b2063b8db
Cristian6600 <cristian6600@hotmail.es>
2023-06-14 17:35:54 -0500

    range-date8

--- applications/daviplata/views.py
+++ applications/daviplata/views.py
@@ -331,7 +331,7 @@ class DashVinculacionView(ListView):
         queryset = User.objects.filter(
             roles = 3, 
             ).filter(Q(user_vinculacion__fecha_visita__range = [kword, kword2])| Q (user_vinculacion__tipo_gestion = tipo)
-            ).annotate(vincula=Count('user_vinculacion'))
+            ).annotate(vincula=Count('user_vinculacion')).order_by('vincula')
         return queryset
         
     def get_context_data(self, **kwargs):
